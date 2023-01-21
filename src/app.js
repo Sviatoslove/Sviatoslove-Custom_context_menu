fc943394d9dc3a75af3570586fd88759aeca2c92
Ivanov Stepan <stepivand@gmail.com>
2023-01-22 04:05:58 +0500

    bug fixes

--- src/app.js
+++ src/app.js
@@ -3,3 +3,8 @@ import './styles.css'
 import { SoundsModule } from './modules/sounds.module'
 
 const randomSounds = new SoundsModule('sounds', 'случайный звук')
+
+// document.addEventListener('click', () => {
+//   randomSounds.trigger()
+// })
+// Проверка работы souns.module
