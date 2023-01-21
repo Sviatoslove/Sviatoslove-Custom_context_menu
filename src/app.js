4b17b647f655e7a49edc2e2826f7f29a04621dcb
Ilia.Levin <krenkbaus.99@mail.ru>
2023-01-21 14:36:04 +0300

    add module custom messege

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 import './styles.css'
 
+import { Messege } from './modules/messege.module'
+
+const getMessege = new Messege('messege', 'Высплывающее сообщение')
+
+getMessege.toHTML()
+getMessege.trigger('Друзей выбираем мы сами, но лучших оставляет время')
